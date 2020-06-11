040f3822c5ebaa5ddc1146d0c4533441d7ffd13b
Jingxiang Guo <jguo10@uh.edu>
2020-06-11 12:17:09 -0500

    start tracking tutorial.py

---
+++ tutorial.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+x = np.arange(1,10,0.05) 
+
+y= np.sin(x) 
+
+plt.plot(x,y) 
+
+plt.show() 
+
+xxx.gg 
+
+# Figure 2  
+
+def xxx():
+
+    a= 2 
+
+    return None 
+
